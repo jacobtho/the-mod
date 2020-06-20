cb4a592fbfebe7bc97dba506524a3216ab59417a
jacobtho <jacob@DESKTOP-16QJJEO>
2020-06-20 19:17:18 -0400

    stuff

---
+++ src/main/java/net/mcreator/modernitemsmod/item/LegoItem.java
@@ -0,0 +1,46 @@
+
+package net.mcreator.modernitemsmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.modernitemsmod.ModernItemsModModElements;
+
+@ModernItemsModModElements.ModElement.Tag
+public class LegoItem extends ModernItemsModModElements.ModElement {
+	@ObjectHolder("modern_items_mod:lego")
+	public static final Item block = null;
+	public LegoItem(ModernItemsModModElements instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64));
+			setRegistryName("lego");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
